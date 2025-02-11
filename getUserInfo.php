bc0f880a2376237bf2ed670166913834d28ad4ea
Jessie <cic38553@pennwest.edu>
2025-02-11 10:55:55 -0500

    Journaling functions added
    Journal pages are linked with user session data carrying over to each page. Hidden fields were created in journaljournaling.html to hold emotion data which will be implemented and added to the journal entry in the next sprint.
    ReadMe has been updated

--- getUserInfo.php
+++ getUserInfo.php
@@ -2,10 +2,22 @@
 session_start();
 header('Content-Type: application/json');
 
-$response = array('username' => '');
+$response = array('username' => '', 'role' => '', 'journals' => array());
 
 if (isset($_SESSION['username'])) {
-    $response['username'] = $_SESSION['username'];
+    $username = $_SESSION['username'];
+    $response['username'] = $username;
+    $response['role'] = $_SESSION['role'];
+
+    // Fetch user's journal entries from the database
+    require 'db_connect.php'; // Include your database connection file
+    try {
+        $stmt = $db->prepare("SELECT journal_content, journal_date, emotional_intensity_rating FROM Journal_Entry WHERE user_id = (SELECT user_id FROM Users WHERE user_username = ?)");
+        $stmt->execute([$username]);
+        $response['journals'] = $stmt->fetchAll(PDO::FETCH_ASSOC);
+    } catch (PDOException $e) {
+        $response['message'] = 'Error: ' . $e->getMessage();
+    }
 }
 
 echo json_encode($response);
