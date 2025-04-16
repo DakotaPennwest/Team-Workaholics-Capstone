ba69cd3cd6290c218358274b647f7de016259d3a
JessieC <cic38553@pennwest.edu>
2025-04-15 21:42:07 -0400

    Update journalSelection.js

--- scripts/journalSelection.js
+++ scripts/journalSelection.js
@@ -5,7 +5,7 @@ document.addEventListener("DOMContentLoaded", function() {
     const selectedEmotionNameInput = document.getElementById('selectedEmotionNameInput');
     const selectedEmotionValueInput = document.getElementById('selectedEmotionValueInput');
     const emotionIdInput = document.getElementById('emotionIdInput');
-
+    
     radioButtons.forEach(radio => {
         radio.addEventListener('change', function() {
             if (this.checked) {
@@ -13,23 +13,23 @@ document.addEventListener("DOMContentLoaded", function() {
                 const emotionName = parentLabel.getAttribute('title');
                 const emotionValue = this.value;
                 const emotionId = this.dataset.emotionid;
-
+                
                 // Update UI elements
                 document.getElementById("selectedEmotionEmoji").src = `./images/emotions/${emotionValue}.svg`;
                 document.getElementById("selectedEmotionName").innerText = emotionName;
-
+                
                 // Update hidden inputs
                 selectedEmotionNameInput.value = emotionName;
                 selectedEmotionValueInput.value = emotionValue;
                 emotionIdInput.value = emotionId;
-
+                
                 nextButtonContainer.classList.remove('hidden');
                 console.log("Hidden input emotionId value:", emotionIdInput.value);
             }
         });
     });
-
-   // Check primary emotion counts on page load
+    
+    // Check primary emotion counts on page load
     const primaryButtons = document.querySelectorAll('.emotion-option.main input[type="radio"]');
     
     function checkPrimaryEmotion(button) {
