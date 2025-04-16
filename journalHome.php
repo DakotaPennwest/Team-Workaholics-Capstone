de25cdf4d1d6e3c4417bb0d4a5c420d4b4b50f60
JessieC <cic38553@pennwest.edu>
2025-04-15 21:02:27 -0400

    Update journalHome.php

--- journalHome.php
+++ journalHome.php
@@ -47,7 +47,7 @@ if (!isset($_SESSION['username'])) {
                 <span class="navigation-bar-link-text">Home</span>
             </a>
             <!-- Journal Link -->
-            <a href="journalHome.html" class="navigation-bar-link">
+            <a href="journalHome.php" class="navigation-bar-link">
                 <img src="./images/icons/journalIcon.svg" alt="Journal Icon" class="navigation-bar-link-icon">
                 <span class="navigation-bar-link-text-selected">Journal</span>
             </a>
