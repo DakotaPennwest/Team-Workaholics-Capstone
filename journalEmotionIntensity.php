8ceff0ac2f406993e2111c55ac4a7f73f8949864
JessieC <cic38553@pennwest.edu>
2025-04-15 21:01:39 -0400

    Update journalEmotionIntensity.php
    corrected path to journalHome.php

--- journalEmotionIntensity.php
+++ journalEmotionIntensity.php
@@ -39,11 +39,11 @@ $emotionId    = $_SESSION['journalEntry']['emotionId'] ?? '1';
         <img src="./images/icons/homeIcon.svg" alt="Home Icon" class="navigation-bar-link-icon">
         <span class="navigation-bar-link-text">Home</span>
       </a>
-      <a href="journalHome.html" class="navigation-bar-link">
+      <a href="journalHome.php" class="navigation-bar-link">
         <img src="./images/icons/journalIcon.svg" alt="Journal Icon" class="navigation-bar-link-icon">
         <span class="navigation-bar-link-text-selected">Journal</span>
       </a>
-      <a href="#" class="navigation-bar-link">
+      <a href="strategiesHome.html" class="navigation-bar-link">
         <img src="./images/icons/strategyIcon.svg" alt="Strategies Icon" class="navigation-bar-link-icon">
         <span class="navigation-bar-link-text">Strategies</span>
       </a>
